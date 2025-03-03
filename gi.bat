526c4372cc9da5052f1a1932cf0b1b7a3140cfff
Ivan Office <guohuadeng@hotmail.com>
2025-03-03 22:40:24 +0800

    init odooai repo

---
+++ gi.bat
@@ -0,0 +1,3 @@
+title init Greenodoo18 x64 fast - www.odooai.cn
+rd /s/q .\source-git
+git clone -b master https://github.com/odoo/odoo.git .\source-git
---
+++ gu.bat
@@ -0,0 +1,7 @@
+title update Greenodoo18 x64 fast - www.odooai.cn
+cd source_git/odoo
+git pull
+cd..
+cd enterprise
+git pull
+cd..
